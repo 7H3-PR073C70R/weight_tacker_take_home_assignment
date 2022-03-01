938362eadb7b122b9d1760e689a1f3e4f1aed58d
Okanlawon .D Oluwatobi <57195156+toxic-bishop@users.noreply.github.com>
2022-03-01 16:01:42 +0000

    Delete lib directory

--- lib/constants/app_colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-class AppColors {
-  static const kcButtonBackgroundColor = Color.fromARGB(255, 54, 57, 59);
-  static const kcWhiteColor = Color.fromARGB(255, 255, 255, 255);
-  static const kcErrorColor = Color.fromARGB(255, 223, 26, 12);
-}
